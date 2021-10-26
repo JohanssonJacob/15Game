717954b00691c126327bf86023e1ba739e5de361
JohanssonJacob <jacobjohanssn2001@gmail.com>
2021-10-26 21:18:25 +0200

    Initial commit

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -1,13 +1,9 @@
 package com.company;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
-import java.util.Random;
 
 public class GUI extends JFrame implements ActionListener {
 
